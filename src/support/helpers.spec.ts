310c06d6395a37b9912244cdd9a91678dd51c136
Chris Shaw <me@cshaw.xyz>
2020-06-22 23:28:25 +1000

    Added additional helper tests

--- src/support/helpers.spec.ts
+++ src/support/helpers.spec.ts
@@ -1,6 +1,13 @@
 import {
+  setupEnvironment,
+} from '../config'
+
+import { Environment } from '../types'
+
+import {
   configurationProxy,
   generateConfiguration,
+  getIfUtilsInstance,
 } from './helpers'
 
 describe('helpers', () => {
@@ -21,6 +28,25 @@ describe('helpers', () => {
     const environment = { mode: 'development' }
 
     // @ts-expect-error
-    expect(generateConfiguration<(env: typeof environment) => void>((env) => env.mode, environment)).toEqual('development')
+    expect(generateConfiguration<(env: typeof environment) => string>((env) => env.mode, environment))
+      .toEqual('development')
+  })
+
+  test('if utilities should be in mock mode', () => {
+    const environment: Environment = setupEnvironment({
+      mock    : true,
+      prod    : false,
+      project : 'mock_project',
+    })
+
+    // @ts-expect-error
+    environment.mode = 'mock'
+
+    expect(getIfUtilsInstance().ifDevelopment(true, false)).toBe(false)
+
+    // TODO: Work out how to hook into `envVars` so we can create custom utility behaviours
+    // expect(getIfUtilsInstance().ifMock(true, false)).toBe(true)
+
+    expect(environment.mode).toEqual('mock')
   })
 })
